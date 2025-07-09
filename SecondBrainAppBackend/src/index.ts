de861f30eb11246ab815f5755b7e9e82edaa8765
Om_Ratnaparkhe <ratnaparakheom04@gmail.com>
2025-07-09 21:48:01 +0530

    CORS changes

--- SecondBrainAppBackend/src/index.ts
+++ SecondBrainAppBackend/src/index.ts
@@ -13,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: 'https://brainly-app-mern-59bq.vercel.app/',
+   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 app.post("/api/v1/signup",async(req,res)=>{
